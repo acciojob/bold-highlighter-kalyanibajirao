e17595a5b508c8f69a26d91f6765617381f9f078
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-06-02 12:58:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
     //Write your code here
-
+  for (let i = 0; i < boldItems.length; i++) {
+        boldItems[i].classList.add('highlighted');
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+for (let i = 0; i < boldItems.length; i++) {
+        boldItems[i].classList.remove('highlighted');
+    }
     
 }
